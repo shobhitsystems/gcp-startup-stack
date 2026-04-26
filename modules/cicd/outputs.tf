6d2f6153646456d9ee4b12796f483ebee88e5a51
Shobhit Systems <111685352+shobhitsystems-owner@users.noreply.github.com>
2026-04-26 11:02:28 +0530

    Delete modules/cicd directory

--- modules/cicd/outputs.tf
+++
@@ -1,1 +0,0 @@
-output "trigger_name" { value = length(google_cloudbuild_trigger.deploy) > 0 ? google_cloudbuild_trigger.deploy[0].name : "no-trigger-github-not-configured" }
--- modules/cicd/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "project_id"        { type = string }
-variable "region"            { type = string }
-variable "env"               { type = string }
-variable "registry_host"     { type = string }
-variable "cloud_run_service" { type = string }
-variable "github_owner"      { type = string; default = "" }
-variable "github_repo"       { type = string; default = "" }
